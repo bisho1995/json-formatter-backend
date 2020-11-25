9b32a852e5829e171354890a59600aff1ffb76b3
Bisvarup Mukherjee <bisvarup.m@flipkart.com>
2020-11-26 02:19:35 +0530

    feat: get single json working

--- src/resources/json/JsonResource.js
+++ src/resources/json/JsonResource.js
@@ -3,20 +3,36 @@ const debug = require("debug")("server:jsonResource");
 const {
   sequelize: { models },
 } = require("../../database");
+const randomstring = require("randomstring");
 
 const JsonResource = function JsonResource() {
   const router = express.Router();
 
   router.post("/jsons", this.addJson);
-  router.get("/jsons", this.getJson);
+  router.get("/jsons", this.getAllJson);
+  router.get("/jsons/:url", this.getJson);
 
   return router;
 };
 
 JsonResource.prototype.getJson = async function getJson(req, res) {
+  const Json = models.Json;
+  const { url } = req.params;
+  try {
+    const jsons = await Json.findAll({ attributes: ["json"], where: { url } });
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).send(jsons);
+  } catch (error) {
+    res.status(500).send();
+  }
+};
+
+JsonResource.prototype.getAllJson = async function getAllJson(req, res) {
   try {
     const Json = models.Json;
-    const jsons = await Json.findAll();
+    const jsons = await Json.findAll({ attributes: ["url", "json"] });
+
+    res.setHeader("Content-Type", "application/json");
     return res.status(200).send(jsons);
   } catch (error) {
     return res.status(500);
@@ -43,12 +59,16 @@ JsonResource.prototype.addJson = async function addJson(req, res) {
   if (!invalidJson) {
     try {
       const Json = models.Json;
-      await Json.create({ json, url: Date.now() });
+      const url = randomstring.generate(8);
+      await Json.create({ json, url });
+
+      return res
+        .status(200)
+        .json({ url: `http://localhost:8080/jsons/${url}` });
     } catch (error) {
       debug("Could not json to DB %O", err);
       return res.status(500).send();
     }
-    return res.status(200).send();
   } else {
     debug("bad json request");
     res.status(400).send();
