bb2e74bad50a69e293d1d21b1a6818ab8668e9a1
Bisvarup Mukherjee <bisvarup.m@flipkart.com>
2020-11-26 02:04:14 +0530

    feat: get and add json routes working

--- src/resources/json/JsonResource.js
+++ src/resources/json/JsonResource.js
@@ -1,15 +1,58 @@
 const express = require("express");
+const debug = require("debug")("server:jsonResource");
+const {
+  sequelize: { models },
+} = require("../../database");
 
 const JsonResource = function JsonResource() {
   const router = express.Router();
 
-  router.post("/json", this.addJson);
+  router.post("/jsons", this.addJson);
+  router.get("/jsons", this.getJson);
 
   return router;
 };
 
-JsonResource.prototype.addJson = function addJson(req, res) {
-  res.send("working");
+JsonResource.prototype.getJson = async function getJson(req, res) {
+  try {
+    const Json = models.Json;
+    const jsons = await Json.findAll();
+    return res.status(200).send(jsons);
+  } catch (error) {
+    return res.status(500);
+  }
+};
+
+JsonResource.prototype.addJson = async function addJson(req, res) {
+  debug("in addJson");
+
+  let { json } = req.body;
+  let invalidJson = false;
+
+  if (!json) invalidJson = true;
+  else if (typeof json === "string") {
+    try {
+      json = JSON.parse(json);
+    } catch (error) {
+      invalidJson = true;
+    }
+  } else if (typeof json !== Object) {
+    invalidJson = true;
+  }
+
+  if (!invalidJson) {
+    try {
+      const Json = models.Json;
+      await Json.create({ json, url: Date.now() });
+    } catch (error) {
+      debug("Could not json to DB %O", err);
+      return res.status(500).send();
+    }
+    return res.status(200).send();
+  } else {
+    debug("bad json request");
+    res.status(400).send();
+  }
 };
 
 exports = module.exports = new JsonResource();
